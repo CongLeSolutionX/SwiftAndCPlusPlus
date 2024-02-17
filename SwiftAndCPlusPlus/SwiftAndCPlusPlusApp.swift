6333c899f4a8b306bd46475ebeded868a8d2fc83
Cong Le <longchike@gmail.com>
2024-02-17 12:27:28 -0500

    - Initial setup

---
+++ SwiftAndCPlusPlus/SwiftAndCPlusPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAndCPlusPlusApp.swift
+//  SwiftAndCPlusPlus
+//
+//  Created by Cong Le on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAndCPlusPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
